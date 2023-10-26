440011d0369ddc74f1254f8ae64ef763d750c3ba
Joelle  Johnson <joellej221@gmail.com>
2023-10-26 12:32:12 -0400

    connected to sample api

--- src/components/CoffeeList.jsx
+++ src/components/CoffeeList.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
+
 export default function CoffeeList() {
+    const [coffees, setCoffees] = useState()
+    const getCoffees = (temperature) => {
+        fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+            .then(res => res.json())
+            .then(setCoffees)
+            .catch(alert)
+    }
     return (
         <main>
-            <h2>Coffee List</h2>
+            <div>
+                <button onClick={() => getCoffees('hot')}>Hot</button>
+                <button onClick={() => getCoffees('iced')}>Iced</button>
+            </div>
+            <section>
+                {coffees && 
+                
+                coffees.map(coffee => (
+                    <div key={coffee.id} class="coffee-card">
+                        <img src={coffee.image} alt={coffee.title} />
+                        <h2>{coffee.title}</h2>
+                        <p>{coffee.description}</p>
+                    </div>
+                ))
+                }
+            </section>
         </main>
     )
 }
