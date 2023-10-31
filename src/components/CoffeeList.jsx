d017d182e90c4a925fa06346b8d79d1b233713fc
Joelle  Johnson <joellej221@gmail.com>
2023-10-30 20:03:02 -0400

    incorporated useEffect to connect with API

--- src/components/CoffeeList.jsx
+++ src/components/CoffeeList.jsx
@@ -1,29 +1,45 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function CoffeeList() {
     const [coffees, setCoffees] = useState()
-    const getCoffees = (temperature) => {
-        fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+    const [button, setButton] = useState('Iced')
+    const [temp, setTemp] = useState('hot')
+
+    useEffect(() => {
+        fetch(`https://api.sampleapis.com/coffee/${temp}`)
             .then(res => res.json())
             .then(setCoffees)
             .catch(alert)
+    }, [temp])
+
+    function buttonClick() {
+        //is temp hot?
+        if(temp == 'hot'){
+            //if yes, button should say Iced
+            setButton('Hot')
+            setTemp('iced')
+        } else {
+            //if not, button should say Hot
+            setButton('Iced')
+            setTemp('hot')
+        }
     }
+
     return (
         <main>
             <div>
-                <button onClick={() => getCoffees('hot')}>Hot</button>
-                <button onClick={() => getCoffees('iced')}>Iced</button>
+                <button onClick={buttonClick}>{`${button}`}</button>
             </div>
             <section>
-                {coffees && 
-                
-                coffees.map(coffee => (
-                    <div key={coffee.id} class="coffee-card">
-                        <img src={coffee.image} alt={coffee.title} />
-                        <h2>{coffee.title}</h2>
-                        <p>{coffee.description}</p>
-                    </div>
-                ))
+                {coffees && //shorthand if in react, doesn't require an else
+
+                    coffees.map(coffee => (
+                        <div key={coffee.id} class="coffee-card">
+                            <img src={coffee.image} alt={coffee.title} />
+                            <h2>{coffee.title}</h2>
+                            <p>{coffee.description}</p>
+                        </div>
+                    ))
                 }
             </section>
         </main>
